c9f213585af546e986bf6d37f95fdeb66d138da1
StarCandy-D2 <kdh04090@gmail.com>
2025-05-22 15:06:40 +0900

    asdf

---
+++ Assets/Scripts/PlayerCondition.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IDamagalbe
+{
+    void TakePhygicalDamage(int damage);
+}
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition hunger { get { return uiCondition.hunger; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    public event Action onTakeDamage;
+    void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (hunger.curValue <= 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+        if (health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+    public void Die()
+    {
+        Debug.Log("»ç¸Á");
+    }
+
+    public void TakePhygicalDamage(int damage)
+    {
+        health.Subtract(damage);
+        onTakeDamage?.Invoke();
+    }
+}
